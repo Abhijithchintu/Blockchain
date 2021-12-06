eb108d121b98a1d6deebe821cea228bd53462067
Abhijithchintu <72204219+Abhijithchintu@users.noreply.github.com>
2021-12-06 21:43:23 +0530

    Update royalty.sol

--- royalty.sol
+++ royalty.sol
@@ -1,35 +1,27 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
 import 'https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/utils/erc165.sol';
 
 import './IERC2981Royalties.sol';
 
-/// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
+//This contract is to add ERC2981 support to 1155 and ERC721  
 abstract contract ERC2981PerTokenRoyalties is ERC165, IERC2981Royalties {
+
+  
     struct Royalty {
-        address recipient;
+        address artist;
         uint256 value;
     }
 
-    mapping(uint256 => Royalty) internal _royalties;
+    mapping(uint256 => Royalty) internal royalties; //creating a map of royalties
 
-
-    /// @dev Sets token royalties
-    /// @param id the token id fir which we register the royalties
-    /// @param recipient recipient of the royalties
-    /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
-    function _setTokenRoyalty(
-        uint256 id,
-        address recipient,
-        uint256 value
-    ) internal {
-        require(value <= 10000, 'ERC2981Royalties: Too high');
-
-        _royalties[id] = Royalty(recipient, value);
+    //this is a function for setting Royalty (struct)
+    function _setTokenRoyalty(uint256 id, address artist,uint256 value) internal {
+        require(value <= 10000, 'ERC2981Royalties is high ');
+        royalties[id] = Royalty(artist, value);
     }
 
-    /// @inheritdoc IERC2981Royalties
+    // this is override function for royaltyinfo 
     function royaltyInfo(uint256 tokenId, uint256 value)
         external
         view
@@ -39,4 +31,12 @@ abstract contract ERC2981PerTokenRoyalties is ERC165, IERC2981Royalties {
         Royalty memory royalty = _royalties[tokenId];
         return (royalty.recipient, (value * royalty.value) / 10000);
     }
+}
+
+//this is for royalty value , which we wanted to give to artist, it has artist and his art share(royalty)
+interface IERC2981Royalties {
+    function royaltyInfo(uint256 tokenId, uint256 value)
+        external
+        view
+        returns (address _artist, uint256 _royaltyAmount);
 }
